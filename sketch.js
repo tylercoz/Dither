89d80215f57293d0e981c03f382d18e245657c9d
Tyler Cozzocrea <tylercozzocrea@gmail.com>
2022-10-18 17:57:09 -0700

    Ok this is better dithering

--- sketch.js
+++ sketch.js
@@ -14,6 +14,15 @@ function draw() {
                   3, 11, 1,  9, 
                   15, 7, 13, 5];
 
+  // let threshold= [0,48,12,60, 3,51,15,63,
+  //                 32,16,44,28,35,19,47,31,
+  //                  8,56, 4,52,11,59, 7,55,
+  //                 40,24,36,20,43,27,39,23,
+  //                  2,50,14,62, 1,49,13,61,
+  //                 34,18,46,30,33,17,45,29,
+  //                 10,58, 6,54, 9,57, 5,53,
+  //                 42,26,38,22,41,25,37,21];
+
   let cellWidth = threshold.length/2;
   let cellAmount = width / cellWidth;
 
@@ -28,7 +37,7 @@ function draw() {
           let pixelY = (cellY * cellWidth) + y;
 
           let index = (pixelX + pixelY * width) * 4;
-          let k = (x + y * cellWidth) / 4;
+          let k = (x + y * cellWidth/4);
           // console.log(k);
           let c = grayscale(index);
           let thresholdOffset = 256 / threshold.length;
